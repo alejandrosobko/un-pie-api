12917ee59489b4e1257c15b2dbb78a0ddb4a26eb
alejandrosobko <alee-sk8@hotmail.com>
2017-02-27 17:04:14 -0300

    Send time as string

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -47,7 +47,7 @@ RSpec.describe ProductsController, type: :controller do
 
     it 'creates one product' do
       provider = FactoryGirl.create(:provider, name: 'Super calzado')
-      time = Time.new(2017, 01, 01).in_time_zone
+      time = '2017-01-01'
       params = {article: 'OJ1', color: 'Negro', provider: provider.as_json, purchase_date: time}
       post :create, params: {product: params}
       json = JSON.parse(response.body)['product']
