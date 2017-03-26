174fde8565fe9f9e2e2cf3bb22d91a39f1ad7c9c
alejandrosobko <alee-sk8@hotmail.com>
2017-03-26 04:39:24 -0300

    Enhanced style code in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :products, only: [:index, :show, :update] do
-    get 'products/logs', :logs, on: :collection
-  end
-
-  resources :providers, only: [:index, :update, :destroy]do
-    get 'providers/logs', :logs, on: :collection
-  end
-
-  resources :services, only: [:index, :create, :destroy]do
-    get 'services/logs', :logs, on: :collection
-  end
-
-  resources :purchase_orders, only: [:index, :show, :create]do
-    get 'purchase_orders/logs', :logs, on: :collection
-  end
-
-  resources :sales, only: [:index, :show, :create] do
-    get 'sales/earnings', :earnings, on: :collection
-  end
-
-  resources :users, only: [:index, :show, :create, :update] do
-    get 'users/logs', :logs, on: :collection
-  end
+  resources :products, only: [:index, :show, :update, :logs]
+  resources :providers, only: [:index, :update, :destroy, :logs]
+  resources :services, only: [:index, :create, :destroy, :logs]
+  resources :purchase_orders, only: [:index, :show, :create, :logs]
+  resources :sales, only: [:index, :show, :create, :earnings]
+  resources :users, only: [:index, :show, :create, :update, :logs]
 
   post 'user_token' => 'user_token#create'
 
