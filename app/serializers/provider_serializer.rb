468685687ad6cb9618158b6e411ea50e8e23f0ec
alejandrosobko <alee-sk8@hotmail.com>
2017-04-22 18:36:55 -0300

    modified provider serializer

--- app/serializers/provider_serializer.rb
+++ app/serializers/provider_serializer.rb
@@ -1,10 +1,10 @@
 class ProviderSerializer < ActiveModel::Serializer
-  attributes :id, :name, :products
+  attributes :id, :name, :products, :stocks
 
   def products
     @object.products.map do |product|
       product_json = product.as_json
-      product_json[:amount] = Stock.find_by(product_id: product.id, provider_id: @object.id).amount
+      product_json[:amount] = nil
       product_json
     end
   end
