54ad6c9e94a5f5d8b6cd963cb36a8a013dd1b24b
alejandrosobko <alee-sk8@hotmail.com>
2017-04-22 13:21:26 -0300

    Provider serializer adds stock to each product

--- app/serializers/provider_serializer.rb
+++ app/serializers/provider_serializer.rb
@@ -1,5 +1,11 @@
 class ProviderSerializer < ActiveModel::Serializer
-  attributes :id, :name
-  has_many :products, each_serializer: ProductSerializer
+  attributes :id, :name, :products
 
+  def products
+    @object.products.map do |product|
+      product_json = product.as_json
+      product_json[:amount] = Stock.find_by(product_id: product.id, provider_id: @object.id).amount
+      product_json
+    end
+  end
 end
